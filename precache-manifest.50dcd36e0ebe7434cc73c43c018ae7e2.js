d56f1da60c2380e9a8788d573e19ad008e91f33e
unknown <c.o.n.s.t.a.n.t.tnatsnoc@gmail.com>
2019-10-20 12:30:30 +0300

    Updates

---
+++ precache-manifest.50dcd36e0ebe7434cc73c43c018ae7e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a5d0974019c99bb38e9fd3a1b01faf",
+    "url": "/ReactSocial/index.html"
+  },
+  {
+    "revision": "b54cca34d8c89df70794",
+    "url": "/ReactSocial/static/css/main.ad3128b3.chunk.css"
+  },
+  {
+    "revision": "19226352c1ded3b24b17",
+    "url": "/ReactSocial/static/js/2.c16e616c.chunk.js"
+  },
+  {
+    "revision": "b54cca34d8c89df70794",
+    "url": "/ReactSocial/static/js/main.a7b0663b.chunk.js"
+  },
+  {
+    "revision": "65312f063da419988a4f",
+    "url": "/ReactSocial/static/js/runtime-main.f62263b9.js"
+  },
+  {
+    "revision": "c5c60cfafff5e00e7568d411005d7980",
+    "url": "/ReactSocial/static/media/logo.c5c60cfa.png"
+  }
+]);
